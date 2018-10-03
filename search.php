629bc1e5d471847dd6522b9604d897bad7fddde7
ptavarez16 <ptavarez16@yahoo.com>
2018-10-02 21:23:00 -0400

    Add search page - New page that takes the input from the search bar and returns data

---
+++ search.php
@@ -0,0 +1,70 @@
+<?php include "includes/db.php";?>
+<?php include "includes/header.php";?>
+<!-- Navigation -->
+  <?php include "includes/navigation.php";?>
+  <!-- Page Content -->
+  <div class="container">
+
+      <div class="row">
+
+          <!-- Blog Entries Column -->
+          <div class="col-md-8">
+            
+            <?php
+              if(isset($_POST['search'])) {
+                $search = $_POST['search'];
+                
+                $query = "SELECT * FROM posts WHERE tags LIKE '%$search%'";
+                $search_query = mysqli_query($connection, $query);
+                
+                if(!$search_query) {
+                  die('Query Failed' . mysqli_error($connection));
+                }
+                
+                $count = mysqli_num_rows($search_query);
+                
+                if($count == 0) {
+                  echo "<h5>Search not found...</h5>";
+                } else {
+                  while($row = mysqli_fetch_assoc($search_query)) {
+                    $post_title = $row['title'];
+                    $post_author = $row['author'];
+                    $post_date = $row['date'];
+                    $post_image = $row['image'];
+                    $post_content = $row['content'];
+                    
+                    ?>
+                    <h1 class="page-header">
+                        Page Heading
+                        <small>Secondary Text</small>
+                    </h1>
+
+                    <!-- Blog Posts -->
+                    <h2>
+                        <a href="#"><?php echo $post_title;?></a>
+                    </h2>
+                    <p class="lead">
+                        by <a href="index.php"><?php echo $post_author;?></a>
+                    </p>
+                    <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date;?></p>
+                    <hr>
+                    <img class="img-responsive" src="images/<?php echo $post_image;?>" alt="">
+                    <hr>
+                    <p><?php echo $post_content;?></p>
+                    <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+                    <hr>
+                <?php }
+                }
+              }
+              ?>
+          </div>
+
+          <!-- Blog Sidebar Widgets Column -->
+          <?php include "includes/sidebar.php";?>
+
+      </div>
+      <!-- /.row -->
+
+      <hr>
+<?php include "includes/footer.php";?>
