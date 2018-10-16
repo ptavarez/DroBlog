1a02a7a672f9aeec9a94d00a4242277aaf6f45da
ptavarez16 <ptavarez16@yahoo.com>
2018-10-15 22:02:05 -0400

    Remove Container and Other Modifications

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,45 +1,43 @@
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-    <div class="container">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="index.php">DroBlog</a>
-        </div>
-        <!-- Blog Search Well -->
-        <form class="navbar-form navbar-left" action="search.php" method="post">
-          <div class="input-group">
-            <input name="search" type="text" class="form-control" placeholder="Search">
-            <span class="input-group-btn">
-              <button class="btn btn-default" type="submit" name="login">
-                <span class="glyphicon glyphicon-search"></span>
-              </button>
-            </span>
-          </div>
-        </form>
-        <!-- /search form -->
+  <!-- Brand and toggle get grouped for better mobile display -->
+  <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.php">DroBlog</a>
+  </div>
+
+  <!-- Collect the nav links, forms, and other content for toggling -->
+  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <!-- Blog Search Well -->
+    <form class="navbar-form navbar-left" action="search.php" method="post">
+      <div class="input-group">
+        <input name="search" type="text" class="form-control" placeholder="Search">
+        <span class="input-group-btn">
+          <button class="btn btn-default" type="submit" name="login">
+            <span class="glyphicon glyphicon-search"></span>
+          </button>
+        </span>
+      </div>
+    </form>
+    <!-- /search form -->
+    
+      <ul class="nav navbar-nav navbar-right" style="margin-right:5px;">
+        <?php
+        $query = "SELECT * FROM categories";
+        $fetchCats = mysqli_query($connection, $query);
         
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-              <?php
-              $query = "SELECT * FROM categories";
-              $fetchCats = mysqli_query($connection, $query);
-              
-              while($row = mysqli_fetch_assoc($fetchCats)) {
-                $cat_id = $row['id'];
-                $cat_title = $row['title'];
-                
-                echo "<li><a href='category.php?category=$cat_id'>{$cat_title}</a></li>";
-              }
-              ?>
-            </ul>
-        </div>
-        <!-- /.navbar-collapse -->
-    </div>
-    <!-- /.container -->
+        while($row = mysqli_fetch_assoc($fetchCats)) {
+          $cat_id = $row['id'];
+          $cat_title = $row['title'];
+          
+          echo "<li><a href='category.php?category=$cat_id'>{$cat_title}</a></li>";
+        }
+        ?>
+      </ul>
+  </div>
+  <!-- /.navbar-collapse -->
 </nav>
