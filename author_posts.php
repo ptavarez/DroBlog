e12bbc2837b2bc3a079a121c454c443fadd92381
ptavarez16 <ptavarez16@yahoo.com>
2018-10-23 19:06:33 -0400

    Author posts - Add file to render posts by a specific user - Quick modifications

--- author_posts.php
+++ author_posts.php
@@ -9,41 +9,39 @@
 
           <!-- Blog Entries Column -->
           <div class="col-md-8">
-            <h1 class="page-header">
-                DroBlog
-                <small>Blog your life.</small>
-            </h1>
             
             <?php
-              $query = "SELECT * FROM posts";
+            if(isset($_GET['p_id'])) {
+              $post_id = $_GET['p_id'];
+              $post_author = $_GET['author'];
+            }
+            ?>
+            <h1 class="page-header">
+                Posts by
+                <small><?php echo $post_author;?></small>
+            </h1>
+            <?php
+              $query = "SELECT * FROM posts WHERE author = '{$post_author}'";
               
               $fetchPosts = mysqli_query($connection, $query);
               
               while($row = mysqli_fetch_assoc($fetchPosts)) {
-                $post_id = $row['id'];
                 $post_title = $row['title'];
                 $post_author = $row['author'];
                 $post_date = $row['date'];
                 $post_image = $row['image'];
-                $post_content = substr($row['content'], 0, 150);
+                $post_content = $row['content'];
                 
                 ?>
-
                 <!-- Blog Posts -->
                 <h2>
-                    <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title;?></a>
+                    <a href="post.php?p_id=<?php echo $post_id;?>"><?php echo $post_title;?></a>
                 </h2>
-                <p class="lead">
-                    by <a href="author_posts.php?author=<?php echo $post_author;?>&p_id=<?php echo $post_id; ?>"><?php echo $post_author;?></a>
-                </p>
                 <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date;?></p>
                 <hr>
-                <a href="post.php?p_id=<?php echo $post_id; ?>">
-                  <img class="img-responsive" src="images/<?php echo $post_image;?>" alt="">
-                </a>
+                <img class="img-responsive" src="images/<?php echo $post_image;?>" alt="">
                 <hr>
                 <p><?php echo $post_content;?></p>
-                <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                 <hr>
             <?php } ?>
@@ -51,7 +49,7 @@
 
           <!-- Blog Sidebar Widgets Column -->
           <?php include "includes/sidebar.php";?>
-
+          
       </div>
       <!-- /.row -->
 
--- post.php
+++ post.php
@@ -26,12 +26,12 @@
                 $post_content = $row['content'];
                 
                 ?>
+                <h1 class="page-header">
+                    <?php echo $post_title;?>
+                </h1>
                 <!-- Blog Posts -->
-                <h2>
-                    <a href="#"><?php echo $post_title;?></a>
-                </h2>
                 <p class="lead">
-                    by <a href="index.php"><?php echo $post_author;?></a>
+                    by <a href="author_posts.php?author=<?php echo $post_author;?>&p_id=<?php echo $post_id; ?>"><?php echo $post_author;?></a>
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date;?></p>
                 <hr>
