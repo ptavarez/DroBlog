84845df9a2a0417b8aa3cd7029f9cbfbad4382c2
ptavarez16 <ptavarez16@yahoo.com>
2018-11-01 20:16:36 -0400

    Coding faculty suggestion update

--- includes/login.php
+++ includes/login.php
@@ -16,32 +16,31 @@
   $query = "SELECT * FROM users WHERE username = '{$username}'";
   $fecthUser = mysqli_query($connection, $query);
   confirm($fecthUser);
- }
  
- while($row = mysqli_fetch_array($fecthUser)) {
-   $user_id = $row['id'];
-   $user_firstname = $row['firstname'];
-   $user_lastname = $row['lastname'];
-   $user_username = $row['username'];
-   $user_password = $row['password'];
-   $user_role = $row['role'];
- }
- echo $password.'<br>';
- echo $user_password.'<br>';
- 
- if(password_verify($password,$user_password)) {
-   echo 'Password verified';
+   while($row = mysqli_fetch_array($fecthUser)) {
+     $user_id = $row['id'];
+     $user_firstname = $row['firstname'];
+     $user_lastname = $row['lastname'];
+     $user_username = $row['username'];
+     $user_password = $row['password'];
+     $user_role = $row['role'];
+   }
+   echo $password.'<br>';
+   echo $user_password.'<br>';
    
-   $_SESSION['id'] = $user_id;
-   $_SESSION['username'] = $user_username;
-   $_SESSION['firstname'] = $user_firstname;
-   $_SESSION['lastname'] = $user_lastname;
-   $_SESSION['role'] = $user_role;
-   
-   // header("Location: ../admin");
-   
- } else {
-   echo 'Login Failed';
-   // header("Location: ../index.php");
- }
+   if(password_verify($password,$user_password)) {
+     echo 'Password verified';
+     if (session_status() == PHP_SESSION_NONE) session_start();
+     $_SESSION['id'] = $user_id;
+     $_SESSION['username'] = $user_username;
+     $_SESSION['firstname'] = $user_firstname;
+     $_SESSION['lastname'] = $user_lastname;
+     $_SESSION['role'] = $user_role;
+     
+     // header("Location: ../admin");
+   } else {
+     echo 'Login Failed';
+     // header("Location: ../index.php");
+   }
+}
 ?>
